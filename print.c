020266352dc10a20b027cf1557be2fb50f0ba498
nox <samingosi97@gmail.com>
2023-09-16 12:08:05 -0700

    make % initialization

--- print.c
+++ print.c
@@ -6,7 +6,16 @@
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
+	int count = 0, i;
 
+	for (i = 0; format[i]; i++)
+	{
+		while (format[i] != '%')
+		{
+			count += _putchar(format[i]);
+			i++;
+			continue;
+		}
+	}
 	return (count);
 }
