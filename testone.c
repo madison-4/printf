791b436b5e1116ea0ec306055fc33ab4cb81a79e
Enox <anzimboingosi@gmail.com>
2023-09-18 16:31:43 +0300

    remove increment inside for loop

--- testone.c
+++ testone.c
@@ -29,16 +29,15 @@ int main(void)
 	len = printf("What happens when it's a percent %% follow");
 	len2 = _printf("What happens when it's a percent %% follow");
 	printf("\nThe std printf returns %d while yours returns %d see the difference",len ,len2);
-*/	
-/*	str = "A simple string";
+	str = "A simple string";
 	len = _printf("Let's conjoin a string %s and see", str);
 	len2 = printf("let's conjoin a string %s and see", str);
 	printf("\nThe std printf returns %d while yours returns %d see the difference",len2,len);
-	str = NULL;
+*/	str = NULL;
 	len = _printf("Now let's see what happens when the string %s is NULL", str);
 	len2 = printf("Now let's see what happens when the string %s is NULL", str);
 	printf("\nThe std printf returns %d while yours returns %d see the difference",len2,len);
-	len = _printf("let's see the return when a percent % is alone in the middle");
+/*	len = _printf("let's see the return when a percent % is alone in the middle");
 	len2 = printf("let's see the return when a percent % is alone in the middle");
 	printf("\nThe std printf returns %d while yours returns %d see the difference",len2,len);
 	len = _printf("Now a percent ends the string %");
