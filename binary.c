861eeb76b5cbb5cb9586641145efe2e338040e09
nox <samingosi97@gmail.com>
2023-10-21 02:29:08 -0700

    debug with gpt

--- binary.c
+++ binary.c
@@ -6,4 +6,29 @@
  */
 int *dec2bin(int n)
 {
+	int *decarr;
+	long unsigned  int i;
+
+	decarr = malloc((sizeof(int) * 8));
+	if (decarr == NULL)
+		return (NULL);
+	for (i = 0; i < (sizeof(int) * 8); i++)
+		decarr[i] = (n << 1);
+	return (decarr);
+}
+/**
+ * main - test dectobin
+ * no arguments
+ * Return: 0 on success
+ */
+int main(void)
+{
+
+	int x = 1024, *arr;
+	size_t i;
+	arr = dec2bin(x);
+	for (i = 0; i <(sizeof(int) * 8); i++)
+		putchar(arr[i] + 48);
+	free(arr);
+	return (0);
 }
