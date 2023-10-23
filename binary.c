94a80413eb698b192037d9cefba354f27bdc4921
enox <anzimboingosi@gmail.com>
2023-10-23 10:23:51 -0700

    swap bits after taking them

--- binary.c
+++ binary.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include<stdio.h>
+
 /**
  * dec2bin - convert an int representation to a binary one
  * @n: integer to convert
@@ -6,14 +8,20 @@
  */
 int *dec2bin(int n)
 {
-	int *decarr, x = 1;
-	size_t i, len = (sizeof(int) * 8) - 1;
+	int *decarr, x = 1, temp, j;
+	size_t i, len = (sizeof(int) * 8);
 
 	decarr = malloc((sizeof(int) * 8) * sizeof(int));
 	if (decarr == NULL)
 		return (NULL);
-	for (i = 0; i < (sizeof(int) * 8); i++, len--)
+	for (i = 0; i < len; i++)
 		decarr[i] = ((n >> i) & x);
+	for (i = 0, j = (len - 1); i < (len / 2); i++, j--)
+	{
+		temp = decarr[j];
+		decarr[j] = decarr[i];
+		decarr[i] = temp;
+	}
 	return (decarr);
 }
 /**
@@ -24,11 +32,15 @@ int *dec2bin(int n)
 int main(void)
 {
 
-	int x = 1024, *arr;
+	int x = 1, *arr;
 	size_t i;
+	
 	arr = dec2bin(x);
-	for (i = 0; i <(sizeof(int) * 8); i++)
-		putchar(arr[i] + 48);
+	if (arr == NULL)
+		exit(EXIT_FAILURE);
+	for (i = 0; i < (sizeof(int) * 8); i++)
+		printf("%d", arr[i]);
+	putchar(10);
 	free(arr);
 	return (0);
 }
